d51e668328b3e73cc750f597aa621e5f90499a87
ercoppa <ercoppa@945a0969-16d2-8fab-a1a4-d2fe7934ba0d>
2011-12-01 15:59:02 +0000

    Reimplemented gradient map

--- gradient_map/src/gradient_map_generator.cpp
+++ gradient_map/src/gradient_map_generator.cpp
@@ -4,24 +4,80 @@
 
 static ros::Publisher gradient;
 
-void distanceCallback(const distance_map::DistanceMap& msg) {
-	std::vector<double> matrix(msg.size_x * msg.size_y);
+#ifndef SQUARE_2
+#define SQUARE_2 1.414213562     
+#endif
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+void distanceCallback(const distance_map::DistanceMap& msg)
+{
 	gradient_map::GradientMap grad;
-	
+	std::vector<double> matrix_scalar(msg.size_x * msg.size_y);
+	std::vector<double> matrix_theta(msg.size_x * msg.size_y);
+	grad.map_scalar = matrix_scalar;
+	grad.map_theta = matrix_theta;
 	grad.size_x = msg.size_x;
 	grad.size_y = msg.size_y;
-	double center = msg.map[msg.size_x*25 + 25];
-	/*
-	double center = msg.map[msg.size_x*(msg.size_y/2 - 1) + msg.size_x/2 - 1];
-	center += msg.map[msg.size_x*(msg.size_y/2 - 1) + msg.size_x/2];
-	center += msg.map[msg.size_x*(msg.size_y/2) + msg.size_x/2 - 1];
-	center += msg.map[msg.size_x*(msg.size_y/2) + msg.size_x/2];
-	center /= 4;
-	*/
-	for (int i = 0; i < msg.size_y*msg.size_x; i++) {
-		matrix[i] = msg.map[i] - center;
+	
+	for (int x = 0; x < msg.size_y; x++) {
+		for (int y = 0; y < msg.size_x; y++) {
+			
+			double center = msg.map[x*msg.size_x + y];
+			double scalar = 1000;
+			double theta = -1;
+			
+			for (int i = x-1; i < x+2; x++) {
+				for (int k = y-1; k < y+2; k++) {
+					
+					/* Invalid indexes */
+					if (i > 0 && i < msg.size_y && k > 0 && k < msg.size_y)
+						continue;
+					/* the center... */
+					if (i == x && k == y)
+						continue;
+					
+					double near = msg.map[i*msg.size_x + k];
+					double delta = 1;
+					if (x != i && y != k) delta = SQUARE_2;
+					
+					double grad = (near - center) / delta;
+					if (grad < scalar) {
+						
+						scalar = grad;
+						if (i < x) {
+							if (k < y) theta = 135;
+							else if (k == y) theta = 90;
+							else theta = 45;
+						} else if (i > x) {
+							if (k < y) theta = 225;
+							else if (k == y) theta = 270;
+							else theta = 315;
+						} else {
+							if (k < y) theta = 180;
+							else theta = 0;
+						}
+						
+					}
+					
+				}
+				
+				if (theta == -1) 
+					ROS_INFO("Invalid gradient");
+					
+				grad.map_scalar[x*msg.size_x + y] = scalar;
+				
+				/* convert in radiant */
+				//theta = theta * (M_PI / 180);
+				grad.map_theta[x*msg.size_x + y] = theta;
+				
+			}
+
+		}
 	}
-	grad.map = matrix;
+	
 	gradient.publish(grad);
 	ros::Rate loop_rate(20);
 	loop_rate.sleep();
