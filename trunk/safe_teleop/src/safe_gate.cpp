916ac47da431a35e5d559a081f43b8c741fb1e99
ercoppa <ercoppa@945a0969-16d2-8fab-a1a4-d2fe7934ba0d>
2011-12-19 16:13:20 +0000

    Broken rotation of target

--- trunk/safe_teleop/src/safe_gate.cpp
+++ trunk/safe_teleop/src/safe_gate.cpp
@@ -1,10 +1,14 @@
 #include <sstream>
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
+#include <sensor_msgs/Joy.h>
 #include "distance_map/DistanceMap.h"
 #include "occupancy_map/OccupancyMap.h"
 #include "safe_teleop/TrajectoryMap.h"
+#include "tf/transform_listener.h"
+#include "sensor_msgs/LaserScan.h"
 #include <math.h>
+#include <unistd.h>
 #include <queue>
 
 #define MAX_ANG 1.8
@@ -14,9 +18,18 @@ using namespace ros;
 
 int size_x = 50;
 int size_y = 50;
+/* Odom value when pressed button */
+double start_x, start_y, start_z;
 
-Subscriber distance_sub, occupancy_sub, vel_sub;
+/* Current odom value */
+double current_x, current_y, current_z;
+
+/* Arrival point (approximation) */
+int target_x, target_y;
+
+Subscriber distance_sub, occupancy_sub, vel_sub, button_sub, odom_sub;
 Publisher vel_pub, vel2_pub, traj_pub;
+tf::TransformListener * listener = 0;
 
 bool enabled = false;
 
@@ -150,13 +163,19 @@ class Cell
 
 void path_generator(Cell * curr)
 {
-	printf("\n\nPATH: ");
+	
+	//printf("\n\nPATH: ");
+	Cell * next_move = curr;
 	while (curr != NULL) {
 		occupancy[curr->get_col()][curr->get_row()] = 10;
-		printf("[%d,%d] ", curr->get_row(), curr->get_col());
+		//printf("[%d,%d] ", curr->get_row(), curr->get_col());
 		curr = curr->get_parent();
+		if (curr != NULL && curr->get_parent() != NULL)
+			next_move = curr;
 	}
-	printf("\n\n");
+	//printf("\n\n");
+	
+	ROS_INFO("Next move: %d %d", next_move->get_row(), next_move->get_col());
 	
 	std::vector<int> matrix(size_x*size_y);
 	for (int j = 0; j < size_y; j++)
@@ -167,8 +186,10 @@ void path_generator(Cell * curr)
 	msg.size_x = size_x;
 	msg.size_y = size_y;
 	msg.map = matrix;
-
 	traj_pub.publish(msg);
+	
+	/* genera comando */
+	
 	
 }
 
@@ -196,38 +217,64 @@ void velCallback(const geometry_msgs::Twist::ConstPtr& msg) {
 }
 
 int i = 0;
-#define SAFE_DISTANCE 8
-#define MAX_PENALITY 20
+#define SAFE_DISTANCE 4
+#define MAX_PENALITY 12
+#define CELL_RESOLUTION 0.1
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 void distanceCallback(const distance_map::DistanceMap::ConstPtr& msg) {
 
-	if (i++ % 300 != 0) {
-		printf(".");
-		fflush(stdout);
-		return;
-	} else printf("\n");
+	if (!enabled) return;
+	
+	double dx = current_x - start_x;
+	double dy = current_y - start_y;
+	double dz = current_z - start_z;
+	
+	if (dx == 0 && dy == 0 && dz == 0) return;
+	
+	double t_x = (25-target_x)*cos(dz) - (25-target_y)*sin(dz);
+	double t_y = (25-target_x)*sin(dz) + (25-target_y)*cos(dz);
+	
+	target_x = 25 - t_x;
+	target_y = 25 + t_y;
+	//target_x = target_x + (dx / CELL_RESOLUTION);
+	//target_y = target_y + (dy / CELL_RESOLUTION);
+	
+	start_x = current_x;
+	start_y = current_y;
+	start_z = current_z;
+	
+	ROS_INFO("Di: %f %f %f - TARGET: %d %d", dx, dy, dz, target_x, target_y);
+	
+	if (target_x < 0 || target_x > 49 || target_y < 0 || target_y > 49)
+		exit(1);
+	
+	return;
 	
 	int size_x = msg->size_x, size_y = msg->size_y;
 	vector<double> map = msg->map;
 	//printf("conv: %f\n", convert(0, 0, map, size_y)); 
 
 	int manhattan[size_x][size_y];
-	manhattan[5][25] = 0;
-	for (int i = 24; i >= 0; i--) {
-		manhattan[5][i] = manhattan[5][i+1] + 1;
+	manhattan[target_x][target_y] = 0;
+	for (int i = target_y -1; i >= 0; i--) {
+		manhattan[target_x][i] = manhattan[5][i+1] + 1;
 	}
 	
-	for (int i = 26; i < size_y; i++) {
-		manhattan[5][i] = manhattan[5][i-1] + 1;
+	for (int i = target_y + 1; i < size_y; i++) {
+		manhattan[target_x][i] = manhattan[target_x][i-1] + 1;
 	}
 	
-	for (int j = 4; j >= 0; j--)
+	for (int j = target_x - 1; j >= 0; j--)
 		for (int i = 0; i < size_y; i++) 
-			manhattan[j][i] = manhattan[5][i] + (5-j);
+			manhattan[j][i] = manhattan[target_x][i] + (target_x-j);
 			
-	for (int j = 6; j < size_x; j++)
+	for (int j = target_x + 1; j < size_x; j++)
 		for (int i = 0; i < size_y; i++)
-			manhattan[j][i] = manhattan[5][i] + (j-5);
+			manhattan[j][i] = manhattan[target_x][i] + (j-target_x);
 	
 	/*
 	for (int i = 0; i < size_x; i++) {
@@ -254,6 +301,7 @@ void distanceCallback(const distance_map::DistanceMap::ConstPtr& msg) {
 		
 		Cell * current_cell = open_set[pq].top();
 		
+		/*
 		printf("Current cell: ( %d , %d ) : F = %.2f - G = %.2f - H = %.2f", 
 			current_cell->get_row(), current_cell->get_col(), 
 			current_cell->get_f(), current_cell->get_g(),
@@ -263,7 +311,7 @@ void distanceCallback(const distance_map::DistanceMap::ConstPtr& msg) {
 		else printf(" - PARENT = ( %d , %d )\n", 
 						current_cell->get_parent()->get_row(),
 						current_cell->get_parent()->get_col());
-		
+		*/
 		if(current_cell->get_h() == 0) {
 			
 			path_generator(current_cell);
@@ -354,12 +402,92 @@ void distanceCallback(const distance_map::DistanceMap::ConstPtr& msg) {
 }
 
 void occupancyCallback(const occupancy_map::OccupancyMap::ConstPtr& msg) {
+	
 	for (int i = 0; i < msg->size_y; i++)
 		for (int j = 0; j < msg->size_x; j++) {
 			if (msg->map[i*msg->size_x+j])
 				occupancy[j][msg->size_y -1 - i] = 1;
 			else occupancy[j][msg->size_y -1 - i] = 0;
 		}
+	
+	if (!enabled) {
+		
+		std::vector<int> matrix(size_x*size_y);
+		for (int j = 0; j < size_y; j++)
+			for (int i = 0; i < size_x; i++)
+				matrix[j*size_x+i] = occupancy[j][i];
+		
+		safe_teleop::TrajectoryMap msg;
+		msg.size_x = size_x;
+		msg.size_y = size_y;
+		msg.map = matrix;
+
+		traj_pub.publish(msg);
+			
+	}
+
+}
+
+void updateOdomCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+{
+	std::string frame_id = msg->header.frame_id;
+	ros::Time t = msg->header.stamp;
+	tf::StampedTransform laserPose;
+	std::string error = "Error :(";
+	if (listener->waitForTransform ("/odom", "base_link", t, ros::Duration(0.5), ros::Duration(0.01), &error)){
+		
+		listener->lookupTransform("/odom", "/base_link", t, laserPose);
+		double yaw,pitch,roll;
+		btMatrix3x3 mat =  laserPose.getBasis();
+		mat.getEulerZYX(yaw, pitch, roll);
+		
+		current_x = laserPose.getOrigin().x();
+		current_y = laserPose.getOrigin().y();
+		current_z = yaw;
+		
+  } else {
+    std::cerr << error << std::endl; 
+  }
+}
+
+void buttonCallback(const sensor_msgs::Joy::ConstPtr& msg)
+{
+	if (msg->buttons[2] > 0) {
+		
+		printf("Enabled autopilot\n");
+		enabled = true;
+		
+		std::string frame_id = msg->header.frame_id;
+		ros::Time t = msg->header.stamp;
+		tf::StampedTransform laserPose;
+		std::string error = "Error :(";
+		if (listener->waitForTransform ("/odom", "base_link", t, ros::Duration(0.5), ros::Duration(0.01), &error)){
+			
+			listener->lookupTransform("/odom", "/base_link", t, laserPose);
+			double yaw,pitch,roll;
+			btMatrix3x3 mat =  laserPose.getBasis();
+			mat.getEulerZYX(yaw, pitch, roll);
+			
+			/* set relative origin */
+			start_x = laserPose.getOrigin().x();
+			start_y = laserPose.getOrigin().y();
+			start_z = yaw;
+			
+			current_x = laserPose.getOrigin().x();
+			current_y = laserPose.getOrigin().y();
+			current_z = yaw;
+			
+			/* set target */
+			target_x = 5;
+			target_y = 25;
+			
+			ROS_INFO("Position: %f %f %f", start_x, start_y, start_z);
+			
+		} else {
+			std::cerr << error << std::endl; 
+		}
+		
+	} 
 }
 
 int main(int argc, char **argv) {
@@ -371,7 +499,11 @@ int main(int argc, char **argv) {
 	vel_pub = n.advertise<geometry_msgs::Twist>("vel", 1);
 	vel2_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
 	traj_pub = n.advertise<safe_teleop::TrajectoryMap>("trajectory_map", 1);
-
+	button_sub = n.subscribe("joy", 1, buttonCallback); 
+	odom_sub = n.subscribe("/base_scan", 100, updateOdomCallback);
+	
+	listener = new tf::TransformListener;
+	
 	spin(); 
 
 	return 0;
