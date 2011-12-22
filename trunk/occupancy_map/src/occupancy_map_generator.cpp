56f762538ea7d6d75a1ccd0129d85aa52227a236
ercoppa <ercoppa@945a0969-16d2-8fab-a1a4-d2fe7934ba0d>
2011-12-22 08:41:40 +0000

    Broken occupancy map

--- trunk/occupancy_map/src/occupancy_map_generator.cpp
+++ trunk/occupancy_map/src/occupancy_map_generator.cpp
@@ -1,10 +1,28 @@
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 #include <occupancy_map/OccupancyMap.h>
+#include "tf/transform_listener.h"
 
 #include <stdio.h>
 #include <math.h>
 #include <unistd.h>
+
+class Wall
+{
+	public:
+		double row;
+		double col;
+		double tx;
+		double ty;
+	
+		Wall(double xx, double yy)
+		{
+			row = xx;
+			col = yy;
+			tx = 25 - row;
+			ty = 25 - col; 
+		}
+};
 
 #define CELL_RESOLUTION 10 /* centimeters */
 #define X_SIZE          50
@@ -15,13 +33,11 @@
 #endif
 
 ros::Publisher obstacle;
-static bool map[X_SIZE*Y_SIZE];
-static int version_map = 0;
+tf::TransformListener * listener = 0;
+std::vector<Wall*> walls;
+double start_x, start_y, start_z;
 
-void init_map() 
-{
-	for (int i = 0; i < X_SIZE*Y_SIZE; i++) map[i] = false;
-}
+#if 0
 
 /* cell (0,0) is top-left corner of the matrix */
 void set_abs_cell(int x, int y, bool obstacle)
@@ -37,32 +53,57 @@ void set_rel_cell(int x, int y, bool obstacle)
 	set_abs_cell(x+(X_SIZE/2), y+(Y_SIZE/2), obstacle);
 }
 
-void print_map()
-{
-	printf("\nMap version: %d\n", version_map++);
-	for (int i = 0; i < X_SIZE +1; i++) {
-		printf("---");
-	}
-	printf("\n");
-	for (int j = Y_SIZE-1; j >= 0; j--) {
-		printf("|");
-		for (int i = 0; i < X_SIZE; i++) {
-			if (j == Y_SIZE/2 && i == X_SIZE /2) printf(" O ");
-			else if (map[j*X_SIZE+i]) printf(" 1 ");
-			else printf("   ");
-		}
-		printf("|\n");
-	}
-	for (int i = 0; i < X_SIZE+1; i++) {
-		printf("---");
-	}
-	printf("\n");
-}
+#endif
 
 void laserCallback(const sensor_msgs::LaserScan& msg)
 {
+	double current_x, current_y, current_z;
+	tf::StampedTransform laserPose;
+	std::string error = "Error :(";
+	if (listener->waitForTransform ("/odom", "base_link", ros::Time(0), ros::Duration(0.5), ros::Duration(0.01), &error)){
+		
+		listener->lookupTransform("/odom", "/base_link", ros::Time(0), laserPose);
+		double yaw,pitch,roll;
+		btMatrix3x3 mat =  laserPose.getBasis();
+		mat.getEulerZYX(yaw, pitch, roll);
+		
+		current_x = (laserPose.getOrigin().x()) * CELL_RESOLUTION;
+		current_y = (laserPose.getOrigin().y()) * CELL_RESOLUTION;
+		current_z = yaw;
+		
+	} else {
+		std::cerr << error << std::endl; 
+	}
+	
+	double dx = sqrt( (current_x-start_x)*(current_x-start_x) + (current_y-start_y)*(current_y-start_y) );
+	double dy = 0; 
+	double dz = current_z - start_z;
+	
+	//if (dx == 0 && dy == 0 && dz == 0) return;
+	dz = -dz;
+	
+	
+	for (int i = 0; i < walls.size(); i++) {
+		
+		double t_x = (walls[i]->col-25)*cos(dz) - (25-walls[i]->row)*sin(dz);
+		double t_y = (walls[i]->col-25)*sin(dz) + (25-walls[i]->row)*cos(dz);
+		
+		walls[i]->row = walls[i]->row + (walls[i]->ty - t_y); //row
+		walls[i]->col = walls[i]->col + (t_x - walls[i]->tx); //col
+		
+		walls[i]->row = walls[i]->row + dx;
+		walls[i]->col = walls[i]->col + dy;
+		walls[i]->tx = t_x - dy;
+		walls[i]->ty = t_y - dx;
+		
+	}
+	
+	start_x = current_x;
+	start_y = current_y;
+	start_z = current_z;
+	
 	/* Clear map */
-	init_map();
+	//init_map();
 	
 	/* Number of scan samples */
 	int n = (msg.angle_max - msg.angle_min) / msg.angle_increment;
@@ -73,17 +114,29 @@ void laserCallback(const sensor_msgs::LaserScan& msg)
 	
 	for (int i = 0; i < n; i++) {
 		
-		double x_rel = (msg.ranges[i] * sin(angle)) * 100;
-		double y_rel = (msg.ranges[i] * cos(angle)) * 100;
+		double x_rel = (msg.ranges[i] * sin(angle)) * CELL_RESOLUTION; /* centimetres */
+		double y_rel = (msg.ranges[i] * cos(angle)) * CELL_RESOLUTION; /* centimetres */
 		
 		/*
 		if (angle < 0 && x_rel >= 0) 
 			printf("(%f, %f) : (%f, %f)\n", x_rel, y_rel, angle, msg.ranges[i]); 
 		*/
 		
+		Wall* new_wall = new Wall(25-y_rel, 25-x_rel);
+		//TODO: verificare se cella già esiste
+		walls.push_back(new_wall);
+		
 		/* scale (x,y) to cell resolution */
-		x_rel /= CELL_RESOLUTION;
-		y_rel /= CELL_RESOLUTION;
+		//x_rel /= CELL_RESOLUTION;
+		//y_rel /= CELL_RESOLUTION;
+		
+		
+		
+		//int x = round(-x_rel);
+		//int y = round(y_rel);
+		
+		/*;;
+		//y_rel /= CELL_RESOLUTION;
 		
 		int x = round(-x_rel);
 		int y = round(y_rel);
@@ -92,24 +145,39 @@ void laserCallback(const sensor_msgs::LaserScan& msg)
 		if (x > -50 && x < 50 && y > -50 && y < 50) 
 			printf("(%d, %d) : (%f, %f)\n", x, y, angle, msg.ranges[i]); 
 		*/
+
+		//y_rel /= CELL_RESOLUTION;
+		
+		//int x = round(-x_rel);
+		//int y = round(y_rel);
+		
+		/*
+		if (x > -50 && x < 50 && y > -50 && y < 50) 
+			printf("(%d, %d) : (%f, %f)\n", x, y, angle, msg.ranges[i]); 
+		*/
+
+		/*
+		if (x > -50 && x < 50 && y > -50 && y < 50) 
+			printf("(%d, %d) : (%f, %f)\n", x, y, angle, msg.ranges[i]); 
+		*/
 		
 		/* Insert it in the map */
-		set_rel_cell(x, y, true);
-		
+		//set_rel_cell(x, y, true);
+
 		//angle += increment; // degree
-		angle += msg.angle_increment;
+		//angle += msg.angle_increment;
 		
 	}
 	
-	//print_map();
-	//exit(0);
+	std::vector<uint8_t> matrix(X_SIZE*Y_SIZE);
+	for(int i = 0; i < walls.size(); i++) {
+		int row = round(walls[i]->row), col = round(X_SIZE-walls[i]->col);
+		matrix[(Y_SIZE-1 - row) * X_SIZE + col] = true;
+	}
 	
 	occupancy_map::OccupancyMap msg_o;
 	msg_o.size_x = X_SIZE;
 	msg_o.size_y = Y_SIZE;
-	std::vector<uint8_t> matrix(X_SIZE*Y_SIZE);
-	for (int i = 0; i < X_SIZE*Y_SIZE; i++)
-		matrix[i] = map[i];
 	msg_o.map = matrix;
 	obstacle.publish(msg_o);
 	
@@ -126,18 +194,7 @@ int main(int argc, char **argv)
 
 	ros::Subscriber laser = n.subscribe("base_scan", 1, laserCallback);
 	obstacle = n.advertise<occupancy_map::OccupancyMap>("occupancy_map", 1);
-	
-	init_map();
-	
-	/*
-	set_abs_cell(10, 10, true);
-	set_abs_cell(20, 20, true);
-	set_abs_cell(30, 30, true);
-	set_abs_cell(40, 40, true);
-	set_rel_cell(0, 0, true); 
-	set_rel_cell(-10, -10, true); 
-	print_map();
-	*/
+	listener = new tf::TransformListener;
 
 	ros::spin();
 
